4bcfc12a6675bc8b445e9b96f7a0959af35627aa
Odinn Adalsteinsson <odinn.adalsteinsson@worksome.com>
2022-05-06 15:47:14 +0200

    Upgraded to support Laravel 9

--- tests/CloudFlare/CloudFlareProviderTest.php
+++ tests/CloudFlare/CloudFlareProviderTest.php
@@ -5,7 +5,7 @@ use Worksome\CdnHeaders\ServerHeadersRepository;
 
 beforeEach(fn () => $this->mock = Mockery::mock(ServerHeadersRepository::class));
 
-it('it should get null as a default country code', function () {
+it('should get null as a default country code', function () {
     $this->mock->shouldReceive('get')->andReturn(null);
     $provider = new CloudFlareProvider($this->mock);
 
@@ -13,7 +13,7 @@ it('it should get null as a default country code', function () {
     expect($provider->hasCountryCode())->toBeFalse();
 });
 
-it('it should return a correct country code if server header present', function () {
+it('should return a correct country code if server header present', function () {
     $this->mock->shouldReceive('get')->andReturn('US');
     $provider = new CloudFlareProvider($this->mock);
 
@@ -21,25 +21,25 @@ it('it should return a correct country code if server header present', function 
     expect($provider->hasCountryCode())->toBeTrue();
 });
 
-it('it should get null as a default connecting ip', function () {
+it('should get null as a default connecting ip', function () {
     $this->mock->shouldReceive('get')->andReturn(null);
     $provider = new CloudFlareProvider($this->mock);
     expect($provider->getConnectingIp())->toBe(null);
 });
 
-it('it should return an ip', function () {
+it('should return an ip', function () {
     $this->mock->shouldReceive('get')->andReturn('127.0.0.1');
     $provider = new CloudFlareProvider($this->mock);
     expect($provider->getConnectingIp())->toBe('127.0.0.1');
 });
 
-it('it should get null as a default ray', function () {
+it('should get null as a default ray', function () {
     $this->mock->shouldReceive('get')->andReturn(null);
     $provider = new CloudFlareProvider($this->mock);
     expect($provider->getTraceId())->toBe(null);
 });
 
-it('it should return ray', function () {
+it('should return ray', function () {
     $this->mock->shouldReceive('get')->andReturn('CF12345679');
     $provider = new CloudFlareProvider($this->mock);
     expect($provider->getTraceId())->toBe('CF12345679');
